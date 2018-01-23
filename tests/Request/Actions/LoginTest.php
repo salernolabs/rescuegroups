a3de90b3bf8db37dec92546bf8dc2fad64f6d2ad
Eric Salerno <eric.salerno@viacom.com>
2018-01-23 16:13:14 -0500

    Test environment variable reading for login tests

--- tests/Request/Actions/LoginTest.php
+++ tests/Request/Actions/LoginTest.php
@@ -26,4 +26,33 @@ class LoginTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('orgid1', $data['accountNumber']);
         $this->assertEquals('login', $data['action']);
     }
+
+    /**
+     * Test environment setup
+     *
+     * @throws \RescueGroups\Exceptions\LoginRequired
+     */
+    public function testEnvironmentSetup()
+    {
+        $username = getenv('RESCUEGROUPS_USERNAME');
+        $password = getenv('RESCUEGROUPS_PASSWORD');
+        $orgId = getenv('RESCUEGROUPS_ORG_ID');
+
+        putenv('RESCUEGROUPS_USERNAME=testenvuser');
+        putenv('RESCUEGROUPS_PASSWORD=testenvpassword');
+        putenv('RESCUEGROUPS_ORG_ID=testenvorgid');
+
+        $request = new \RescueGroups\Request\Actions\Login();
+
+        $data = $this->api->getPostObject($request);
+
+        $this->assertEquals('testenvuser', $data['username']);
+        $this->assertEquals('testenvpassword', $data['password']);
+        $this->assertEquals('testenvorgid', $data['accountNumber']);
+        $this->assertEquals('login', $data['action']);
+
+        putenv('RESCUEGROUPS_USERNAME=' . $username);
+        putenv('RESCUEGROUPS_PASSWORD=' . $password);
+        putenv('RESCUEGROUPS_ORG_ID=' . $orgId);
+    }
 }
